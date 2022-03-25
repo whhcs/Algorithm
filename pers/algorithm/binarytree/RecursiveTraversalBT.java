eabb0eb9ac73dba83d46b7a5ff3d6ee3ce4e02e6
whhom <66947896@qq.com>
2022-03-25 21:15:42 +0800

    二叉树的前中后序遍历（递归版）

---
+++ pers/algorithm/binarytree/RecursiveTraversalBT.java
@@ -0,0 +1,59 @@
+package pers.algorithm.binarytree;
+
+public class RecursiveTraversalBT {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int v) {
+            value = v;
+        }
+    }
+
+    // 先序遍历打印所有节点
+    public static void preOrder(Node head) {
+        if (head == null)
+            return;
+        System.out.println(head.value);
+        preOrder(head.left);
+        preOrder(head.right);
+    }
+
+    // 中序遍历打印所有节点
+    public static void inOrder(Node head) {
+        if (head == null)
+            return;
+        inOrder(head.left);
+        System.out.println(head.value);
+        inOrder(head.right);
+    }
+
+    // 后序遍历打印所有节点
+    public static void postOrder(Node head) {
+        if (head == null)
+            return;
+        postOrder(head.left);
+        postOrder(head.right);
+        System.out.println(head.value);
+    }
+
+    // test
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        preOrder(head);
+        System.out.println("=================");
+        inOrder(head);
+        System.out.println("=================");
+        postOrder(head);
+        System.out.println("=================");
+    }
+}
